f5f5ac8e4c9f3358ef922551084ee77b204560a0
Sania Sagheer <saniasagheer2005@gmail.com>
2026-08-27 00:30:37 +0530

    ✅ A Very Big Sum [python3] — approach1

---
+++ solutions/python3/easy/a-very-big-sum/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true
+# Problem     A Very Big Sum
+# Difficulty  Easy
+# Subdomain   Warmup
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-27, 12:30 a.m.
+# ──────────────────────────────────────────────────
+
+n = int(input())
+ar = list(map(int, input().split()))
+
+print(sum(ar))
