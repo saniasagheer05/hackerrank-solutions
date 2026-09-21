b6cd015d5858e5817fc3c159a2a5ff5d94474128
Sania Sagheer <saniasagheer2005@gmail.com>
2026-09-21 23:08:38 +0530

    ✅ Solve Me First [python3] — approach1

---
+++ solutions/python3/easy/solve-me-first/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+# Problem     Solve Me First
+# Difficulty  Easy
+# Subdomain   Warmup
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-21, 11:08 p.m.
+# ──────────────────────────────────────────────────
+
+a = int(input())
+b = int(input())
+
+print(a + b)
